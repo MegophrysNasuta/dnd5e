a2e6ccaa03369e8c10a17ee2176474259c5877dc
Megophrys Nasuta <nasutater@gmail.com>
2021-12-06 16:53:02 -0600

    Initial commit

---
+++ dnd5e_functools/dicerolls.py
@@ -0,0 +1,169 @@
+import operator
+import random
+import re
+from typing import Callable, Generator, Iterable, Tuple, Union
+
+
+class DieResult:
+    """
+    Stores a max_die_value (e.g. 6 for a 6-sided die) and result, making them
+    resistant to rewrites once set.
+    """
+
+    def __init__(self, die_max_value, result=None):
+        self.__die_max_value = int(die_max_value)
+        self.__result =  None
+        if result is not None:
+            self.result = result
+
+    def reroll(self):
+        return self.__class__(result=random.randint(1, self.die_max_value),
+                              die_max_value=self.die_max_value)
+
+    @property
+    def die_max_value(self):
+        return self.__die_max_value
+
+    @property
+    def result(self):
+        return self.__result
+
+    @result.setter
+    def result(self, x):
+        if self.__result is None:
+            self.__result = int(x)
+        else:
+            raise AttributeError("can't set attribute")
+
+    def __float__(self):
+        return float(self.result)
+
+    def __int__(self):
+        return int(self.result)
+
+    def __add__(self, value):
+        return int(self) + int(value)
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __eq__(self, value):
+        return int(self) == float(value)
+
+    def __ge__(self, value):
+        return int(self) >= float(value)
+
+    def __gt__(self, value):
+        return int(self) > float(value)
+
+    def __le__(self, value):
+        return int(self) <= float(value)
+
+    def __lt__(self, value):
+        return int(self) < float(value)
+
+    def __ne__(self, value):
+        return int(self) != float(value)
+
+    def __repr__(self):
+        if self.result:
+            return '<DieResult: %i (d%i)>' % (self.result, self.die_max_value)
+        else:
+            return '<DieResult: Undecided>'
+
+
+def roll_dice(mDn: str,
+              rerolling_if: Iterable[str]=None,
+              dropping_lowest: bool=False) -> Generator[DieResult, None, None]:
+    """
+    Syntactic sugar for roll with other functions as options.
+
+    roll_dice('4d6', rerolling_if=('x==1',))
+    is equivalent to
+    reroll_if(roll('4d6'), ('x==1',))
+
+    roll_dice('4d6', rerolling_if='x==1', dropping_lowest=True)
+    is equivalent to
+    drop_lowest(reroll_if(roll('4d6'), 'x==1'))
+    """
+    rolls = roll(mDn)
+    if rerolling_if:
+        rolls = reroll_if(rolls, rerolling_if)
+    if dropping_lowest:
+        rolls = drop_lowest(rolls)
+    return rolls
+
+
+def roll(mDn: str) -> Generator[DieResult, None, None]:
+    """
+    Roll mDn, a standard dice signifier like '3d6' (m=3, n=6)
+
+    Returns a DieResult, like <DieResult: 4 (d6)>
+
+    Raises ValueError or TypeError on improper mDn formatting.
+    """
+    if not mDn:
+        raise ValueError('Must specify a die roll like "3d6"')
+
+    m, n = map(int, str(mDn).split('d', 1))
+    for die_roll in range(m):
+        yield DieResult(die_max_value=n, result=random.randint(1, n))
+
+
+def parse_condition(condition: str) -> Tuple[Callable[[int, int], bool], int]:
+    """
+    Parse a string representing a comparison like 'x < 1', returning
+    a function representing the operation, and the int value 1,
+    in the example case.
+
+    If a string like '1 > x' is used, it will be converted to 'x < 1' first.
+    """
+    condition_regex = re.compile(r'^(x|\d+) ?(<|>|<=|>=|==|!=) ?(x|\d+)')
+    matches = condition_regex.match(str(condition))
+    if not matches:
+        raise ValueError('Unrecognized condition string')
+
+    if matches[1] == 'x':
+        op, val = matches[2], int(matches[3])
+    else:
+        op, val = matches[2], int(matches[1])
+
+    op_dict = {
+        '<': operator.lt,
+        '<=': operator.le,
+        '==': operator.eq,
+        '!=': operator.ne,
+        '>=': operator.ge,
+        '>': operator.gt,
+    }
+
+    opposite_op = {'<': '>', '<=': '>=', '==': '==',
+                   '>': '<', '>=': '<=', '!=': '!=',}
+
+    try:
+        if matches[1] == 'x':
+            real_op = op_dict[op]
+        else:
+            real_op = op_dict[opposite_op[op]]
+    except KeyError:
+        raise ValueError('Unrecognized operation "%s"' % op)
+
+    return real_op, val
+
+
+def reroll_if(rolls: Iterable[DieResult],
+              conditions: Union[str, Iterable[str]]) -> Iterable[DieResult]:
+    if isinstance(conditions, str):
+        conditions = (conditions,)
+
+    for roll in rolls:
+        if any(op(roll.result, val) for op, val in map(parse_condition, conditions)):
+            yield roll.reroll()
+        else:
+            yield roll
+
+
+def drop_lowest(rolls: Iterable[DieResult]) -> Iterable[DieResult]:
+    result = list(rolls)
+    result.remove(min(result))
+    return result
---
+++ dnd5e_functools/test_dicerolls.py
@@ -0,0 +1,267 @@
+import operator
+import pytest
+
+from dicerolls import parse_condition, roll, reroll_if
+
+
+def test_roll():
+    results = tuple(map(int, roll('1d1')))
+    assert len(results) == 1
+    assert results[0] == 1
+
+    result = tuple(roll('1d1'))[0]
+    assert result.result == 1
+    assert result.die_max_value == 1
+
+    with pytest.raises(AttributeError):
+        result.result = 2
+
+    with pytest.raises(AttributeError):
+        result.die_max_value = 2
+
+    results = tuple(map(int, roll('2d6')))
+    assert len(results) == 2
+    assert all(1 <= x <= 6 for x in results)
+
+    results = tuple(map(int, roll('3d5')))
+    assert len(results) == 3
+    assert all(1 <= x <= 5 for x in results)
+
+    results = tuple(map(int, roll('4d20')))
+    assert len(results) == 4
+    assert all(1 <= x <= 20 for x in results)
+
+    results = tuple(map(int, roll('0d0')))
+    assert len(results) == 0
+
+    with pytest.raises(ValueError):
+        list(roll(None))
+
+    with pytest.raises(ValueError):
+        list(roll(1))
+
+
+def test_reroll():
+    results = tuple(reroll_if(roll('4d6'), 'x<2'))
+    assert len(results) == 4
+
+    r1, r2, r3 = roll('3d1')
+    r1_, r2_, r3_ = reroll_if((r1, r2, r3), 'x>1')
+    assert r1 is r1_
+    assert r2 is r2_
+    assert r3 is r3_
+
+    r1_, r2_, r3_ = reroll_if((r1, r2, r3), 'x==1')
+    assert r1 is not r1_
+    assert r2 is not r2_
+    assert r3 is not r3_
+
+    r1_, r2_, r3_ = reroll_if((r1, r2, r3), ('x>0', 'x==3'))
+    assert r1 is not r1_
+    assert r2 is not r2_
+    assert r3 is not r3_
+
+    r1_, r2_, r3_ = reroll_if((r1, r2, r3), ('x<0', 'x==3'))
+    assert r1 is r1_
+    assert r2 is r2_
+    assert r3 is r3_
+
+
+def test_parse_condition():
+    op, val = parse_condition('x < 1')
+    assert op == operator.lt
+    assert val == 1
+
+    op, val = parse_condition('x > 1')
+    assert op == operator.gt
+    assert val == 1
+
+    op, val = parse_condition('x <= 1')
+    assert op == operator.le
+    assert val == 1
+
+    op, val = parse_condition('x >= 1')
+    assert op == operator.ge
+    assert val == 1
+
+    op, val = parse_condition('x == 1')
+    assert op == operator.eq
+    assert val == 1
+
+    op, val = parse_condition('x != 1')
+    assert op == operator.ne
+    assert val == 1
+
+    op, val = parse_condition('x<1')
+    assert op == operator.lt
+    assert val == 1
+
+    op, val = parse_condition('x>1')
+    assert op == operator.gt
+    assert val == 1
+
+    op, val = parse_condition('x<=1')
+    assert op == operator.le
+    assert val == 1
+
+    op, val = parse_condition('x>=1')
+    assert op == operator.ge
+    assert val == 1
+
+    op, val = parse_condition('x==1')
+    assert op == operator.eq
+    assert val == 1
+
+    op, val = parse_condition('x!=1')
+    assert op == operator.ne
+    assert val == 1
+
+    op, val = parse_condition('x < 16')
+    assert op == operator.lt
+    assert val == 16
+
+    op, val = parse_condition('x > 16')
+    assert op == operator.gt
+    assert val == 16
+
+    op, val = parse_condition('x <= 16')
+    assert op == operator.le
+    assert val == 16
+
+    op, val = parse_condition('x >= 16')
+    assert op == operator.ge
+    assert val == 16
+
+    op, val = parse_condition('x == 16')
+    assert op == operator.eq
+    assert val == 16
+
+    op, val = parse_condition('x != 16')
+    assert op == operator.ne
+    assert val == 16
+
+    op, val = parse_condition('x<16')
+    assert op == operator.lt
+    assert val == 16
+
+    op, val = parse_condition('x>16')
+    assert op == operator.gt
+    assert val == 16
+
+    op, val = parse_condition('x<=16')
+    assert op == operator.le
+    assert val == 16
+
+    op, val = parse_condition('x>=16')
+    assert op == operator.ge
+    assert val == 16
+
+    op, val = parse_condition('x==16')
+    assert op == operator.eq
+    assert val == 16
+
+    op, val = parse_condition('x!=16')
+    assert op == operator.ne
+    assert val == 16
+
+    op, val = parse_condition('1 < x')
+    assert op == operator.gt
+    assert val == 1
+
+    op, val = parse_condition('1 > x')
+    assert op == operator.lt
+    assert val == 1
+
+    op, val = parse_condition('1 <= x')
+    assert op == operator.ge
+    assert val == 1
+
+    op, val = parse_condition('1 >= x')
+    assert op == operator.le
+    assert val == 1
+
+    op, val = parse_condition('1 == x')
+    assert op == operator.eq
+    assert val == 1
+
+    op, val = parse_condition('1 != x')
+    assert op == operator.ne
+    assert val == 1
+
+    op, val = parse_condition('1<x')
+    assert op == operator.gt
+    assert val == 1
+
+    op, val = parse_condition('1>x')
+    assert op == operator.lt
+    assert val == 1
+
+    op, val = parse_condition('1<=x')
+    assert op == operator.ge
+    assert val == 1
+
+    op, val = parse_condition('1>=x')
+    assert op == operator.le
+    assert val == 1
+
+    op, val = parse_condition('1==x')
+    assert op == operator.eq
+    assert val == 1
+
+    op, val = parse_condition('1!=x')
+    assert op == operator.ne
+    assert val == 1
+
+    op, val = parse_condition('16 < x')
+    assert op == operator.gt
+    assert val == 16
+
+    op, val = parse_condition('16 > x')
+    assert op == operator.lt
+    assert val == 16
+
+    op, val = parse_condition('16 <= x')
+    assert op == operator.ge
+    assert val == 16
+
+    op, val = parse_condition('16 >= x')
+    assert op == operator.le
+    assert val == 16
+
+    op, val = parse_condition('16 == x')
+    assert op == operator.eq
+    assert val == 16
+
+    op, val = parse_condition('16 != x')
+    assert op == operator.ne
+    assert val == 16
+
+    op, val = parse_condition('16<x')
+    assert op == operator.gt
+    assert val == 16
+
+    op, val = parse_condition('16>x')
+    assert op == operator.lt
+    assert val == 16
+
+    op, val = parse_condition('16<=x')
+    assert op == operator.ge
+    assert val == 16
+
+    op, val = parse_condition('16>=x')
+    assert op == operator.le
+    assert val == 16
+
+    op, val = parse_condition('16==x')
+    assert op == operator.eq
+    assert val == 16
+
+    op, val = parse_condition('16!=x')
+    assert op == operator.ne
+    assert val == 16
+
+    with pytest.raises(ValueError):
+        parse_condition('x * 1')
+
+    with pytest.raises(ValueError):
+        parse_condition('x + y')
